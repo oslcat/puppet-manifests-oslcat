87d78d705e05bcffe1b4d1891348d93ce65ac20f
Mike Cooper <mythmon@gmail.com>
2012-03-17 09:59:26 -0700

    Make vim install gvim on arch, but only if Xorg is isntalled.

--- modules/vim/manifests/params.pp
+++ modules/vim/manifests/params.pp
@@ -1,29 +1,14 @@
 class vim::params {
     $packagename = $::operatingsystem ? {
-        /Archlinux/ => "gvim",
+        /Archlinux/ => has_package('xorg-server') ? {
+            true => "gvim",
+            default => "vim",
+        },
+        # EC2's Amazon Linux is weird.
         /Linux/     => $::ec2_instance_id ? {
             /.+/    => "vim-enhanced",
             default => "vim",
         },
         default     => "vim",
-    }
-
-    $verxorg = package_version('xorg-server')
-    $hasxorg = has_package('xorg-server')
-    $verherp = package_version('herp')
-    $hasherp = has_package('herp')
-
-    notify {"notify-package-version-xorg":
-        message => "xorg-version: ${verxorg}",
-    }
-    notify {"notify-package-has-xorg":
-        message => "have xorg: ${hasxorg}",
-    }
-
-    notify {"notify-package-version-herp":
-        message => "herp-version: ${verherp}",
-    }
-    notify {"notify-package-has-herp":
-        message => "have herp: ${hasherp}",
     }
 }
