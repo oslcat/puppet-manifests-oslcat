32d0fbe086bf614265e9bd2b966f3222017e75a1
Mike Cooper <mythmon@osuosl.org>
2012-03-27 10:42:24 -0700

    Add no sudo option to apply.

--- apply.sh
+++ apply.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sudo puppet apply --modulepath=modules --verbose $* manifests/site.pp
+if [[ ${SUDO} == 'NO' ]]; then
+    SUDO=""
+else
+    SUDO="sudo"
+fi
+
+${SUDO} puppet apply --modulepath=modules --verbose $* manifests/site.pp
