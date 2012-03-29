3aa680dc5e92973d27694f4dcd652adb03bbd283
uberj <uberj@onid.orst.edu>
2012-03-28 18:49:29 -0700

    X fixes and deleting some files.

--- modules/git/manifests/config.pp
+++
@@ -1,9 +0,0 @@
-# $title is the name of the user this config should exist for
-define git::config ($home="/home/${title}") {
-
-    file { "${title}-gitconfig":
-        path    => "${home}/.gitconfig",
-        ensure  => present,
-        content => template("git/gitconfig.erb")
-    }
-}
--- modules/x11/manifests/params.pp
+++ modules/x11/manifests/params.pp
@@ -2,13 +2,16 @@ class x11::params {
   $package_mesa = $::operatingsystem ? {
     /Archlinux/ => "mesa",
     /Ubuntu/    => "mesa-utils",
+    default     => "",
   }
   $package_xorg = $::operatingsystem ? {
     /Archlinux/ => "xorg-server",
     /Ubuntu/    => "xserver-xorg",
+    /Fedora/    => "xorg-x11-server-Xorg",
   }
   $package_xinit = $::operatingsystem ? {
     /Archlinux/ => "xorg-xinit",
     /Ubuntu/    => "xinit",
+    /Fedora/    => "xorg-x11-xinit",
   }
 }
