45b3597e01dad0372bb62a34d9b33e930b995d58
Mike Cooper <mythmon@gmail.com>
2012-04-01 13:58:58 -0400

    Refactor vim::user_config to take content from a parameter.

--- modules/users/manifests/uberj.pp
+++ modules/users/manifests/uberj.pp
@@ -14,7 +14,9 @@ class users::uberj {
     #os::user_config          { "${username}": }
     bash::user_config         { "${username}": }
     git::user_config         { "${username}": }
-    vim::user_config          { "${username}": }
     ruby::dev::user_config   { "${username}": }
     puppet::dev::user_config { "${username}": }
+    vim::user_config { "${username}": 
+        vimrc => templates("users/uberj/vimrc.erb"),
+    }
 }
