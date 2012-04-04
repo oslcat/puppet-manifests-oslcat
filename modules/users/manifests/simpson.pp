d7368d7b73339a9387ccd383c0fadbef421e3748
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-04-03 19:33:50 -0700

    Redent.

--- modules/users/manifests/simpson.pp
+++ modules/users/manifests/simpson.pp
@@ -1,21 +1,21 @@
 class users::simpson {
-    $username = "simpson"
+  $username = "simpson"
 
     user { "${username}":
-        ensure     => present,
-        home       => "/home/${username}",
-        gid        => "users",
-        groups     => ['users', 'wheel'],
-        managehome => true,
-        shell      => '/bin/bash',
+      ensure     => present,
+      home       => "/home/${username}",
+      gid        => "users",
+      groups     => ['users', 'wheel'],
+      managehome => true,
+      shell      => '/bin/bash',
     }
 
-    include fonts::inconsolata
+  include fonts::inconsolata
 
-    vim::user_config         { "${username}":
-      template => "users/simpson/vimrc.erb",
-    }
+  vim::user_config         { "${username}":
+    template => "users/simpson/vimrc.erb",
+  }
 
-    ruby::dev::user_config   { "${username}": }
-    puppet::dev::user_config { "${username}": }
+  ruby::dev::user_config   { "${username}": }
+  puppet::dev::user_config { "${username}": }
 }
