a5adfd1d86c449d78fde95947937816b7eddf65b
Mike Cooper <mythmon@gmail.com>
2012-04-11 13:37:17 -0700

    Fix up os module.

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,1 +1,2 @@
 class bc { package { "bc": ensure => latest; }}
+class pacman { package { "pacman": ensure => latest; }}
--- modules/os/manifests/init.pp
+++ modules/os/manifests/init.pp
@@ -1,27 +1,7 @@
 class os {
   case $::operatingsystem {
     /Archlinux/: {
+      include os::arch
     }
-  }
-}
-
-define os::user_config($home="/home/${title}") {
-
-  case $::operatingsystem {
-    /Archlinux/: {
-      @file { "${title}-arch-zshrc":
-        path    => "${home}/.zshrc.d/arch",
-        tag     => "zsh",
-        owner   => "${title}",
-        group   => "users",
-        content => template('os/arch/zshrc'),
-        ensure  => present;
-      }
-    }
-  }
-
-  @file { "uc-${title}-dot-config":
-    path   => "${home}/.config",
-    ensure => directory,
   }
 }
