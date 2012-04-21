967ee94993823a4c4375ddc397465ad0a23c2f1f
Mike Cooper <mythmon@osuosl.org>
2012-04-21 16:50:37 +0000

    Add comments about has_package and package_version functions.
    Mainly how they can't possibly work, because they run on the server, not the client.
    The only reason they appear to work is because we only use puppet appy, so its fine.

--- modules/util/lib/puppet/parser/functions/has_package.rb
+++ modules/util/lib/puppet/parser/functions/has_package.rb
@@ -1,4 +1,7 @@
 module Puppet::Parser::Functions
+  # This will only work because we are using only 'puppet apply'.  Functions
+  # always run on the puppet master. In our case it happens to be the same as
+  # the client, so this works.
   newfunction(:has_package, :type => :rvalue) do |args|
     name = args.shift
     e = Puppet::Resource.indirection.find("package/#{name}")[:ensure]
--- modules/util/lib/puppet/parser/functions/package_version.rb
+++ modules/util/lib/puppet/parser/functions/package_version.rb
@@ -1,4 +1,7 @@
 module Puppet::Parser::Functions
+  # This will only work because we are using only 'puppet apply'.  Functions
+  # always run on the puppet master. In our case it happens to be the same as
+  # the client, so this works.
   newfunction(:package_version, :type => :rvalue) do |args|
     package_name = args.shift
     Puppet::Resource.indirection.find("package/#{package_name}")[:ensure]
