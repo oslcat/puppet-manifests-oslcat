c52213c3e74752415f22da558c28127c1ccdd93f
Mike Cooper <mythmon@gmail.com>
2012-04-24 21:55:12 -0700

    Style on zsh module.

--- modules/zsh/manifests/init.pp
+++ modules/zsh/manifests/init.pp
@@ -7,23 +7,3 @@ class zsh ($system=true) {
 
   File <| tag == "zsh" |>
 }
-
-# config!
-define zsh::user_config ($home="/home/${title}", $template="zsh/zshrc.erb") {
-
-  @file {
-    "${title}_zsh_dir":
-      path   => "${home}/.zsh",
-      tag    => "zsh",
-      owner  => "${title}",
-      group  => "users",
-      ensure => directory;
-    "${title}-zshrc":
-      path    => "${home}/.zshrc",
-      content => template("${template}"),
-      tag     => "zsh",
-      owner   => "${title}",
-      group   => "users",
-      ensure  => present;
-  }
-}
---
+++ modules/zsh/manifests/user_config.pp
@@ -0,0 +1,19 @@
+# config!
+define zsh::user_config ($home="/home/${title}", $template="zsh/zshrc.erb") {
+
+  @file {
+    "${title}_zsh_dir":
+      path   => "${home}/.zsh",
+      tag    => "zsh",
+      owner  => "${title}",
+      group  => "users",
+      ensure => directory;
+    "${title}-zshrc":
+      path    => "${home}/.zshrc",
+      content => template("${template}"),
+      tag     => "zsh",
+      owner   => "${title}",
+      group   => "users",
+      ensure  => present;
+  }
+}
