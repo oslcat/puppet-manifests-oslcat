a0985e607dbf47e39dd6b171c4e27e9f7e0143dd
Spencer Krum <nibz@cat.pdx.edu>
2012-05-02 05:00:51 -0700

    Managing /home/nibz/.puppet/hiera.yaml super manually for now

--- modules/setup/manifests/init.pp
+++ modules/setup/manifests/init.pp
@@ -1,14 +1,19 @@
 class setup{
   package {
-    "hiera":
+    'hiera':
       source   => 'http://web.cecs.pdx.edu/~nibz/rubygems',
       provider => gem,
       ensure   => present,
   }
   package {
-    "hiera-puppet-nibz":
+    'hiera-puppet-nibz':
       source   => 'http://web.cecs.pdx.edu/~nibz/rubygems',
       provider => gem,
       ensure   => present,
+  }
+  file {
+    '/home/nibz/.puppet/hiera.yaml':
+      ensure  => present,
+      content => template('setup/hiera.yaml.erb'),
   }
 }
