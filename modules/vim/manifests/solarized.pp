fb8fa78143eb9c5d3b0b67bf09265cfbeaf8fd4a
Mike Cooper <mythmon@gmail.com>
2012-05-05 11:47:25 -0700

    Oops, forgot to include this file.

---
+++ modules/vim/manifests/solarized.pp
@@ -0,0 +1,12 @@
+# Solarized vim color scheme.
+
+# $title is the name of the user this config should exist for
+define vim::solarized ($home="/home/${title}") {
+
+    @vcsrepo { "${home}/.vim/bundle/solarized":
+        ensure   => present,
+        provider => git,
+        source   => "git://github.com/altercation/vim-colors-solarized.git",
+        tag      => "${title}_pathogen";
+    }
+}
