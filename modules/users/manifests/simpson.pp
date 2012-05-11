7335e81549bd9aaef428f4638d78f89a0a0e357a
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-05-11 00:25:16 -0700

    users/manifests/simpson: Lint.

--- modules/users/manifests/simpson.pp
+++ modules/users/manifests/simpson.pp
@@ -1,9 +1,9 @@
-class users::simpson ($username="simpson") {
-  if $system {
-    user { "${username}":
+class users::simpson ($username='simpson') {
+  if $::system {
+    user { $username:
       ensure     => present,
       home       => "/home/${username}",
-      gid        => "users",
+      gid        => 'users',
       groups     => ['users', 'wheel'],
       managehome => true,
       shell      => '/bin/bash',
@@ -12,20 +12,20 @@ class users::simpson ($username="simpson") {
     include fonts::inconsolata
   }
 
-  if ($::system) or ($id == $username) {
-    bash::user_config        { "${username}":
-      template => "users/simpson/bashrc.erb",
+  if ($::system) or ($::id == $username) {
+    bash::user_config        { $username:
+      template => 'users/simpson/bashrc.erb',
     }
 
-    git::user_config { "${username}":
-      template => "users/simpson/gitconfig.erb",
+    git::user_config         { $username:
+      template => 'users/simpson/gitconfig.erb',
     }
 
-    vim::user_config         { "${username}":
-      template => "users/simpson/vimrc.erb",
+    vim::user_config         { $username:
+      template => 'users/simpson/vimrc.erb',
     }
 
-    ruby::dev::user_config   { "${username}": }
-    puppet::dev::user_config { "${username}": }
+    ruby::dev::user_config   { $username: }
+    puppet::dev::user_config { $username: }
   }
 }
