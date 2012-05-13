eaea022cbf8bdd3c75555d538d01ce5463378f18
Jesusaurus <jesusaurus@inbox.com>
2012-05-12 19:15:43 -0700

    Adding Jesusaurus' laptop.

---
+++ modules/users/manifests/jesusaurus.pp
@@ -0,0 +1,34 @@
+class users::jesusaurus (
+  $home = "/home/jesusaurus",
+) inherits users {
+
+  user { 'jesusaurus':
+    ensure   => present,
+    home     => "$home",
+    shell    => "/bin/zsh",
+    comment  => "RAWR",
+    groups   => [
+      "audio",
+      "video",
+      "wheel",
+    ],
+  }
+
+  vcsrepo { "$home/.files/":
+    provider => git,
+    ensure   => latest,
+    source   => "git://github.com/jesusaurus/dotFiles.git",
+    revision => "$hostname",
+    notify   => Exec[ "update-config" ],
+    require  => User[ 'jesusaurus' ],
+  }
+
+  exec { "update-config":
+    cwd         => "$home/.files/",
+    path        => "$home/.files/",
+    command     => "./link.sh yes",
+    refreshonly => true,
+    subscribe   => Vcsrepo[ "$home/.files/" ],
+  }
+
+}
