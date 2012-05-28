449e81b0dc5a68b611490661d41d48c6f36508b2
Spencer Krum <nibz@interceptor.nibalizer.com>
2012-05-28 03:34:36 -0700

    Adding stuff to hiera.yaml and munging nibz zsh stuff

--- modules/users/manifests/nibz.pp
+++ modules/users/manifests/nibz.pp
@@ -67,63 +67,56 @@ class users::nibz($user=true) {
 
       "${username}-func":
         path    => "${home}/.zshrc.d/00_func",
-        content => template("users/mythmon/zsh/00_func.erb"),
+        content => template("users/$username/zsh/00_func.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
         ensure  => present;
       "${username}-01_hash_color":
         path    => "${home}/.zshrc.d/01_hash_color",
-        content => template("users/mythmon/zsh/01_hash_color.erb"),
+        content => template("users/$username/zsh/01_hash_color.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
         ensure  => present;
       "${username}-02_signals":
         path    => "${home}/.zshrc.d/02_signals",
-        content => template("users/mythmon/zsh/02_signals.erb"),
+        content => template("users/$username/zsh/02_signals.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
         ensure  => present;
       "${username}-10_autoterm":
         path    => "${home}/.zshrc.d/10_autoterm",
-        content => template("users/mythmon/zsh/10_autoterm.erb"),
+        content => template("users/$username/zsh/10_autoterm.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
         ensure  => present;
       "${username}-20_auto_ls":
         path    => "${home}/.zshrc.d/20_auto_ls",
-        content => template("users/mythmon/zsh/20_auto_ls.erb"),
+        content => template("users/$username/zsh/20_auto_ls.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
         ensure  => present;
       "${username}-50_git":
         path    => "${home}/.zshrc.d/50_git",
-        content => template("users/mythmon/zsh/50_git.erb"),
+        content => template("users/$username/zsh/50_git.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
         ensure  => present;
       "${username}-51_misc":
         path    => "${home}/.zshrc.d/51_misc",
-        content => template("users/mythmon/zsh/51_misc.erb"),
-        tag     => "zsh",
-        owner   => $username,
-        group   => "users",
-        ensure  => present;
-      "${username}-52_ssh":
-        path    => "${home}/.zshrc.d/52_ssh",
-        content => template("users/mythmon/zsh/52_ssh.erb"),
+        content => template("users/$username/zsh/51_misc.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
         ensure  => present;
       "${username}-90_prompt":
         path    => "${home}/.zshrc.d/90_prompt",
-        content => template("users/mythmon/zsh/90_prompt.erb"),
+        content => template("users/$username/zsh/90_prompt.erb"),
         tag     => "zsh",
         owner   => $username,
         group   => "users",
