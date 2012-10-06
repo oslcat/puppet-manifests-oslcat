deac3a9c25bc8679cf12f819b2d28cc1307f92e7
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-10-06 11:03:59 -0700

    modules/awesome: Add user-configurable rc.lua.

---
+++ modules/awesome/manifests/user_config.pp
@@ -0,0 +1,26 @@
+define awesome::user_config (
+  $home="/home/${title}",
+  $template="awesome/rc.lua.erb"
+) {
+  @file {
+  "${title}-config":
+    tag    => "awesome",
+    path   => "${home}/.config",
+    owner  => "${title}",
+    group  => "users",
+    ensure => directory;
+  "${title}-config-awesome":
+    tag    => "awesome",
+    path   => "${home}/.config/awesome",
+    owner  => "${title}",
+    group  => "users",
+    ensure => directory;
+  "${title}-awesomerc":
+    tag     => "awesome",
+    path    => "${home}/.config/awesome/rc.lua",
+    content => template("${template}"),
+    owner   => "${title}",
+    group   => "users",
+    ensure  => present;
+  }
+}
